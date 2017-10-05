2976599888047aa39040348f9d42c7f136fa52d7
PaulFarry <paulfarry@gmail.com>
2017-10-06 05:39:32 +1000

    Initial Load

---
+++ RabbitDequeue/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitDequeue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(" Press [enter] when ready to read.");
+            Console.ReadLine();
+
+            var queueName = "priority";
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var mm = channel.MessageCount(queueName);
+
+                while (true)
+                {
+
+
+                    bool noAck = false;
+                    BasicGetResult result = channel.BasicGet(queueName, noAck);
+                    if (result == null)
+                    {
+                        // No message available at this time.
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        IBasicProperties props = result.BasicProperties;
+                        byte[] body = result.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                        // acknowledge receipt of the message
+                        channel.BasicAck(result.DeliveryTag, false);
+
+                        System.Threading.Thread.Sleep(50);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RabbitEnqueue/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitEnqueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exit = false;
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var queueName = "priority";
+
+            while (!exit)
+            {
+
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: queueName,
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = $"Hello World {Guid.NewGuid().ToString()}!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: queueName,
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+                Console.WriteLine(" Type [Exit | X] to exit, Anything else to repeat.");
+                var result = Console.ReadLine().Trim();
+                if (result.Equals("exit", StringComparison.OrdinalIgnoreCase) || result.Equals("x", StringComparison.OrdinalIgnoreCase))
+                {
+                    exit = true;
+                }
+            }
+        }
+    }
+}
