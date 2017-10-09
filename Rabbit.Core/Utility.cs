2408be7cac0505caa9e3b1ae68f6a030b799ea6b
PaulFarry <paulfarry@gmail.com>
2017-10-09 20:52:27 +1000

    Missing files added

---
+++ Rabbit.Core/Utility.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Rabbit.Core
+{
+    public class Utility
+    {
+        public static byte[] Serialise(BaseRequest request)
+        {
+            using (var ms = new MemoryStream())
+            {
+                ProtoBuf.Serializer.Serialize(ms, request);
+                return ms.ToArray();
+            }
+        }
+        public static BaseRequest DeSerialise(byte[] data)
+        {
+            using (var ms = new MemoryStream(data))
+            {
+                return ProtoBuf.Serializer.Deserialize<BaseRequest>(ms);
+            }
+        }
+    }
+}
